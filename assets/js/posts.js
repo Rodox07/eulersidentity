0090ffbcb71f555f7a0cbb996910b4956c498568
Rodox07 <49347190+Rodox07@users.noreply.github.com>
2026-01-05 01:32:17 -0300

    Add files via upload

---
+++ assets/js/posts.js
@@ -0,0 +1,52 @@
+export const POSTS = [
+  {
+    id: "2026-01-02-presentacion",
+    title: "Introducción",
+    date: "2026-01-02",
+    tags: ["yo"],
+    lede: "Un poco sobre mí.",
+    cover: "/assets/img/intro.jpeg",
+    coverAlt: "Intro",
+    content: String.raw`
+    <article>
+    <p>Me llamo Rodolfo, actualmente tengo 19 años. Cree este blog para documentar todas las cosas que haga este año, y a futuro supongo.</p>
+    <p>Actualmente estoy terminando mi cuarto semestre de ingenieria civil electrica, ¿En enero? Si, mi universidad estuvo en paro por un par de meses, asi que todavia estoy terminando el semestre que deberia haber terminado ya. Me gust    a crear cosas, aun que aun me falta mucho conocimiento para poder hacer lo que quiero hacer. Acá un resumen rapido de lo que estoy haciendo actualmente:</p>
+    <ul>
+        <li>Estudiando Ingenieria Civil Electrica</li>
+        <li>Trabajando en pequeños proyectos con el SCoPE Lab UOH</li>
+        <li>Mentor para el club velocista del programa CyT de la fundación Mustakis</li>
+        <li>Ayudante Corrector para el ramo Fisica 2</li>
+    </ul>
+    <p>Fuera de temas profesionales, estoy tratando de todavia encontrar el balance con las cosas que me gustan hacer y lo que tengo que hacer para progresar en la vida, tratando de aprender a ser un adulto joven en pleno 2026. Asi que eso, si algún dia alguien lee esto a futuro, espero que se me vea como alguien amable y feliz!</p>
+    </article>
+    `
+  },
+  {
+    id: "2026-01-02-baterias",
+    title: "Ayudantia de investigación (Baterias)",
+    date: "2026-01-02",
+    tags: ["proyecto","SCoPE Lab"],
+    lede: "Mi primer ayudantia de investigación!",
+    cover: "/assets/img/lab1.jpeg",
+    coverAlt: "Intro",
+    content: String.raw`
+    <article>
+    Por ahora, no quiero hablar de esto, me tiene chato.
+    </article>
+    `
+  },
+  {
+    id: "2026-01-02-fondo",
+    title: "Fondo de investigación para estudiantes de pregrado 2026",
+    date: "2026-01-02",
+    tags: ["proyecto","SCoPE Lab"],
+    lede: "Mi primer fondo de investigación!!!!!!!!",
+    cover: "/assets/img/lab1.jpeg",
+    coverAlt: "Intro",
+    content: String.raw`    
+    <article>
+    Durante el mes de febrero de este año voy a estar trabajando en el laboratorio con mi primer fondo de investigación :333. Mi tema es sobre implementación de tecnicas de Hardware in the loop para simulación de microrredes. Que significa? GRAN PREGUNTA! Yo tampoco lo sé... Aun... Volveré con más información.
+    </article>
+    `
+  },
+];
